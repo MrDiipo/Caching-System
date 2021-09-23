a474427e76e18cc904255848f909871cd172dedd
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-09-23 01:20:22 +0100

    Cache data structure updates.

--- src/main/java/Cache.java
+++ src/main/java/Cache.java
@@ -1,14 +1,22 @@
-import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Future;
 
 public class Cache<KEY,VALUE> {
 
+    private Map<KEY, VALUE> map = new ConcurrentHashMap<>(); // Data structure for the cache
     // Data source connected to this cache
     private DataSource<KEY, VALUE> dataSource;
 
 
     public Future<VALUE> get(KEY key){
 
+        if (map.containsKey(key)){
+
+        } else {
+            return dataSource.get(key); // Returns the key if not contained in the cache
+        }
     }
 
     public Future<Void> set(KEY key, VALUE value){
--- src/main/java/DataSource.java
+++ src/main/java/DataSource.java
@@ -1,2 +1,13 @@
-public class DataSource {
+import java.util.concurrent.Future;
+
+public class DataSource<KEY, VALUE> {
+
+    public Future<VALUE> get(){
+        return null;
+    }
+
+    public Future<Void> persist(KEY key, VALUE value){
+        return null;
+    }
+
 }
